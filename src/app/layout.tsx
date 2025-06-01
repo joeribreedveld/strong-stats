996c2674e3cddec331758996a4242f26de6dc4e2
joeribreedveld <joeri.breedveld123@gmail.com>
2025-06-01 17:48:57 +0200

    fix: build error in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
 import { StrongDataProvider } from "@/context/StrongDataContext";
